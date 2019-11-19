d0cedbeed1ed940eebd8bb2f6fd982d2bab2ee42
Pedro Wanderley <peu-zico>
2019-11-19 09:32:34 -0300

    context file functions

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,28 +1,33 @@
-import React, { Component } from 'react';
-import Product from './Product';
-import Title from './Title';
-import { ProductConsumer } from '../context';
-
+import React, { Component } from "react";
+import Product from "./Product";
+import Title from "./Title";
+import { storeProducts } from "../data";
+import styled from "styled-components";
+import { ProductConsumer } from "../context";
 export default class ProductList extends Component {
+  state = {
+    products: storeProducts
+  };
   render() {
     return (
       <React.Fragment>
-        <div className="py-5">
+        <ProductWrapper className="py-5">
           <div className="container">
             <Title name="our" title="products" />
-
             <div className="row">
               <ProductConsumer>
-                {value=> {
+                {value => {
                   return value.products.map(product => {
                     return <Product key={product.id} product={product} />;
-                  })
+                  });
                 }}
               </ProductConsumer>
             </div>
           </div>
-        </div>
-      </React.Fragment> 
+        </ProductWrapper>
+      </React.Fragment>
     );
   }
 }
+
+const ProductWrapper = styled.section``;
